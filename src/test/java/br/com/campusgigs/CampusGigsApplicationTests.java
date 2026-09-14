f1dc8cd1ddd9dd50cdfce6c369edc370251441fa
Lucas Hideki <hideki.lucas0701@gmail.com>
2026-09-14 19:53:35 -0300

    Initial Commit

---
+++ src/test/java/br/com/campusgigs/CampusGigsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.campusgigs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusGigsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
