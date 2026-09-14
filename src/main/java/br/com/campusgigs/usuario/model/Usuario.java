84fd4c634ba8258cc57aa4d9d258c2b49c54e861
Lucas Hideki <hideki.lucas0701@gmail.com>
2026-09-14 20:42:38 -0300

    feat(wip): adicionando nome das colunas

--- src/main/java/br/com/campusgigs/usuario/model/Usuario.java
+++ src/main/java/br/com/campusgigs/usuario/model/Usuario.java
@@ -14,16 +14,23 @@ public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
     private Long id;
 
+    @Column(name = "nome_usuario")
     private String nome;
 
+    @Column(name = "email")
     private String email;
 
+    @Column(name = "senha")
     private String senha;
 
     //TODO: Adicionar API ViaCEP
+    @Column(name = "endereco")
     private String endereco;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "papel")
     private Papel papel;
 }
